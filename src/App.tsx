5aa96be872b409d4c7473fc20cd63cc38a7b1404
oatsfx <30326975+oatsfx@users.noreply.github.com>
2024-11-06 19:18:38 -0700

    fix: github pages routing
    needs to use a hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import { Layout } from "./components/Layout";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Layout />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
